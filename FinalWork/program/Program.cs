9b2394acc002df23e03a7ab85f235b2d2fb6fe14
Link <stereo_house01@mail.ru>
2022-11-17 23:34:10 +0300

    добавлена папка с кодом решения задачи

---
+++ FinalWork/program/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+string[] arrayString = new string [8] {"1234", "43", "dog", "cat", "world", "big", "3211", "333"}; 
+string[] arrayFinal = new string[arrayString.Length];
+int n = 3;
+void ArraySecondСompletion (string[] arrayString, string[] arrayFinal, int n)
+{
+for (int i = 0; i < arrayString.Length; i++)
+{
+   if (arrayString[i].Length<= n)
+   {
+    arrayFinal[i]=arrayString[i];
+   }
+}
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");  
+  for (int i = 0; i < array.Length-1; i++)
+  {
+    {
+    array = array.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+    Console.Write($"\"{array[i]}\", ");
+   
+    } 
+  } 
+  Console.Write($"\"{array[array.Length-1]}\"");
+  Console.Write("]");  
+}
+ArraySecondСompletion(arrayString, arrayFinal, n);
+PrintArray(arrayString);
+Console.Write(" => ");
+PrintArray(arrayFinal);
